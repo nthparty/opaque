f7f416d6450a8e2a83ef8e4264ea2eeed2957a68
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-10-25 16:54:20 -0600

    fix(tests): Add type assertions in the test-io object

--- test/test-io.ts
+++ test/test-io.ts
@@ -1,4 +1,4 @@
-import type { IO, IOValue } from "../src/types/io"
+import type { IO, IOData, IOValue, Tag } from "../src/types/io"
 
 /*
  *  Client-Server Communications
@@ -9,10 +9,10 @@ const dummy_socket = (computation_id: string): IO => ({
   get: (op_id, tag) => {
     return new Promise(function (resolve) {
       const _tag = computation_id + ':' + op_id + ':' + tag;
-      const mail = mailbox[_tag];
+      const mail = mailbox[_tag] as IOData[typeof tag] | undefined; // TODO: Factor these assertions out
       if (!mail) {
         // console.debug('io.get', _tag, 'not ready');
-        listeners[_tag] = resolve;
+        listeners[_tag] = resolve as (val: IOValue) => void; // TODO: Factor these assertions out
       } else {
         // console.debug('io.get', _tag, mail);
         resolve(mail);
@@ -20,7 +20,7 @@ const dummy_socket = (computation_id: string): IO => ({
       }
     });
   },
-  give: (op_id, tag, msg) => {
+  give: (op_id, tag: Tag, msg: IOValue) => {
     const _tag = computation_id + ':' + op_id + ':' + tag;
     // console.debug('io.give', _tag, msg);
     const listener = listeners[_tag];
